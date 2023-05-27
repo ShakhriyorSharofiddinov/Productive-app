31deeac1320ecb97323867ea3d227d35da7ca28b
Shakhriyor <shaxriyorsharofiddinov@gmail.com>
2023-05-27 17:22:33 +0500

    changed app name and title

--- lib/main/main.dart
+++ lib/main/main.dart
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+      title: 'Productive',
       theme: ThemeData(
       ),
       home: const SplashScreen(),
